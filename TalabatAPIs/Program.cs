da7404ce1ba7d73fdfdd774bf35daecea3d7a60a
iuy <iuy@DESKTOP-4JUT5K5>
2024-04-11 01:10:46 +0200

    Add project files.

---
+++ TalabatAPIs/Program.cs
@@ -0,0 +1,55 @@
+
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+
+namespace TalabatAPIs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webApplicationBuilder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            #region Configure Services
+            webApplicationBuilder.Services.AddControllers();
+            // Register Required Web APIs Services To The DI Container 
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            webApplicationBuilder.Services.AddEndpointsApiExplorer();
+            webApplicationBuilder.Services.AddSwaggerGen();
+            #endregion
+
+
+            var app = webApplicationBuilder.Build();
+
+            #region Configur Kestrel MiddleWares
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            //app.UseRouting();
+            //app.UseEndPoints(endPoints => 
+            //{ 
+            //endPoints.MapControllerRoute (
+            //      name : "default" ,
+            //      pattern : "{controller}/{action}/{id}"
+            //);
+            //endPoints.MapController();
+            //}
+
+
+            app.MapControllers();
+            #endregion
+
+
+
+            app.Run();
+        }
+    }
+}
