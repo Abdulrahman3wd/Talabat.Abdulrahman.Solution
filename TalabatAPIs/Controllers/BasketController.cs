bcdc477eee3b63d04be34c324f8211f7fd10ea6f
Abdulrahman3wd <abdulrahmanawad7861991@gmail.com>
2024-04-22 23:46:20 +0200

    Validate Customer Basket

--- TalabatAPIs/Controllers/BasketController.cs
+++ TalabatAPIs/Controllers/BasketController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Talabat.Core.Entities;
 using Talabat.Core.Repositories.Contract;
+using TalabatAPIs.DTOs;
 using TalabatAPIs.Errors;
 
 namespace TalabatAPIs.Controllers
@@ -10,10 +12,12 @@ namespace TalabatAPIs.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepository;
+        private readonly IMapper _mapper;
 
-        public BasketController(IBasketRepository basketRepository)
+        public BasketController(IBasketRepository basketRepository , IMapper mapper)
         {
             _basketRepository = basketRepository;
+            _mapper = mapper;
         }
 
         [HttpGet("{id}")] // Get : api/basket?id=10
@@ -23,9 +27,10 @@ namespace TalabatAPIs.Controllers
             return Ok(basket ?? new CustomerBasket(id));
         }
         [HttpPost] // Post /api/basket 
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
         {
-           var  createdOrUpdatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var mappedBasket = _mapper.Map<CustomerBasketDto , CustomerBasket>(basket);
+           var  createdOrUpdatedBasket = await _basketRepository.UpdateBasketAsync(mappedBasket);
            if (createdOrUpdatedBasket is null ) return BadRequest(new ApiResponse(400));
            return Ok(createdOrUpdatedBasket);
         }
