24f748a27f572e9fba9ee0fabb52a5516fbdeae1
Abdulrahman3wd <abdulrahmanawad7861991@gmail.com>
2024-04-27 23:23:48 +0300

    Login Endpoint

---
+++ TalabatAPIs/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities.Identity;
+using TalabatAPIs.DTOs;
+using TalabatAPIs.Errors;
+
+namespace TalabatAPIs.Controllers
+{ 
+	public class AccountController : BaseApiController
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager )
+        {
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		[HttpPost("login")]
+		public async Task<ActionResult<UserDto>> Login(LoginDto model)
+		{
+			var user = await _userManager.FindByEmailAsync(model.Email );
+			if (user is null) return Unauthorized(new ApiResponse(401, "Invalid Login"));
+			var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+			if (!result.Succeeded) return Unauthorized(new ApiResponse(401, "Invalid Login"));
+			return Ok(new UserDto()
+			{
+				DisplayName = user.DisplayName,
+				Email = user.Email,
+				Token = "This Will be Tken"
+			});
+
+        }
+    }
+}
---
+++ TalabatAPIs/DTOs/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TalabatAPIs.DTOs
+{
+	public class LoginDto
+	{
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; } = null!;
+		[Required]
+		public string Password { get; set; } = null!;
+	}
+}
