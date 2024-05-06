5f8f35507d32418536a8f22558d49544f4b01e92
Abdulrahman3wd <abdulrahmanawad7861991@gmail.com>
2024-05-06 16:16:18 +0300

    Order Module - Entities

---
+++ Talabat.Core/Entities/Order Aggregate/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+	public class Order : BaseEntity
+	{
+		public string BuyerEmail { get; set; } = null!;
+		public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+		public OrderStatus Status { get; set; } = OrderStatus.Pending;
+		public Address ShippingAddress { get; set; } = null!;
+		//public int DeliveryMethodId { get; set; } //Forign Key
+		public DeliveryMethod DeliveryMethod { get; set; } = null!; //Navigational property[One]
+		public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();  //Navigational property[Many]
+		public decimal Subtotal { get; set; }
+		//[NotMapped]
+		//public decimal Total => Subtotal + DeliveryMethod.Cost; 
+		public decimal GetTotal()=>Subtotal + DeliveryMethod.Cost;
+		public string PaymentIntentId { get; set; } = string.Empty;
+
+
+    }
+}
---
+++ Talabat.Core/Entities/Order Aggregate/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+	public class OrderItem : BaseEntity
+	{
+		public ProductItemOrder Product { get; set; } = null!;
+		public decimal Price { get; set; }
+		public int Quantity { get; set; }	
+	}
+}
