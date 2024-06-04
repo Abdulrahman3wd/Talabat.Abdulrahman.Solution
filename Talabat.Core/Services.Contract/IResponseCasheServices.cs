bf5c2612c9ed89c0345d1003ec62b339be87a250
AbdulrahmanAwad <Abdulrahmanawad7861991@gmail.com>
2024-06-04 11:27:55 +0300

    Cashing Services

---
+++ Talabat.Core/Services.Contract/IResponseCasheServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Services.Contract
+{
+    public interface IResponseCasheServices
+    {
+        Task CasheResponseAsync(string key , object Response , TimeSpan TimeToLive);
+        Task<string?> GetCashedResponseAsync(string key);
+
+
+    }
+}
